f8982652d5003fd1330c57f18fb979dcbfc98d54
wangyuzZZ <2696023004@qq.com>
2020-04-03 09:24:51 +0800

    内容更新

--- util.js
+++ util.js
@@ -868,7 +868,7 @@ function H5() {
  * @param {传参格式：时间戳} nows
  * @param {传参格式：year month day hour minute second} type
  */
-function transformDate(nows, type){
+function transformDate(nows, type) {
     let now = new Date(nows)
     var year = now.getFullYear();
     var month = now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1;
@@ -877,35 +877,29 @@ function transformDate(nows, type){
     var minute = now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes();
     var second = now.getSeconds() < 10 ? '0' + now.getSeconds() : now.getSeconds();
     switch (type) {
-        case 'year':
-            {
-                return year
-            }
-            break;
-        case 'month':
-            {
-                return year + "-" + month
-            }
-            break;
-        case 'day':
-            {
-                return year + "-" + month + "-" + date
-            }
-            break;
-        case 'hour':
-            {
-                return year + "-" + month + '-' + date + ' ' + hour
-            }
-            break;
-        case 'minute':
-            {
-                return year + "-" + month + '-' + date + ' ' + hour + ':' + minute
-            }
-            break;
-        case 'second':
-            {
-                return year + "-" + month + '-' + date + ' ' + hour + ':' + minute + ':' + second
-            }
+        case 'year': {
+            return year
+        }
+        break;
+    case 'month': {
+        return year + "-" + month
+    }
+    break;
+    case 'day': {
+        return year + "-" + month + "-" + date
+    }
+    break;
+    case 'hour': {
+        return year + "-" + month + '-' + date + ' ' + hour
+    }
+    break;
+    case 'minute': {
+        return year + "-" + month + '-' + date + ' ' + hour + ':' + minute
+    }
+    break;
+    case 'second': {
+        return year + "-" + month + '-' + date + ' ' + hour + ':' + minute + ':' + second
+    }
     }
 }
 /**
@@ -933,7 +927,7 @@ function isEmail(email) {
         return false;
     }
 }
- /**
+/**
  * 验证是否为手机号
  */
 function isPoneAvailable(poneInput) {
@@ -946,7 +940,7 @@ function isPoneAvailable(poneInput) {
 }
 /**
  *深拷贝 
-*/
+ */
 function deepCopy(o) {
     if (o instanceof Array) {
         var n = [];
@@ -964,4 +958,184 @@ function deepCopy(o) {
     } else {
         return o;
     }
+}
+/**
+ * QQ截图选择框
+ * Html：
+ * <div class="box" id="box" style="width:200px; height:200px;left:0; top:100px;">
+    <div class="upleftbtn" id="upLeftBtn"></div>
+    <div class="uprightbtn" id="upRightBtn"></div>
+    <div class="downleftbtn" id="downLeftBtn"></div>
+    <div class="downrightbtn" id="downRightBtn"></div>
+    </div>
+    CSS:
+    .box {
+            width: 200px;
+            height: 200px;
+            border: 2px solid gray;
+            position: relative;
+        }
+
+        .upleftbtn,
+        .uprightbtn,
+        .downleftbtn,
+        .downrightbtn {
+            width: 10px;
+            height: 10px;
+            border: 1px solid steelblue;
+            position: absolute;
+            z-index: 5;
+            background: whitesmoke;
+        }
+
+        .upleftbtn {
+            top: -5px;
+            left: -5px;
+            cursor: nw-resize;
+        }
+
+        .uprightbtn {
+            top: -5px;
+            right: -5px;
+            cursor: ne-resize;
+        }
+
+        .downleftbtn {
+            left: -5px;
+            bottom: -5px;
+            cursor: sw-resize;
+        }
+
+        .downrightbtn {
+            right: -5px;
+            bottom: -5px;
+            cursor: se-resize;
+        }
+ */
+function screenshotsBox() {
+    var obj = null; //当前操作对象
+    var box = null; //要处理的对象;
+    //保留上次的x,y位置
+    var clickX = 0;
+    var clickY = 0;
+    // 大盒子
+    document.querySelector('#box').onmousedown = (e) => {
+        onDragDown(e, "move");
+    }
+    // 四个角(小盒子)
+    Array.from(document.querySelectorAll('#box div')).forEach(item => {
+        item.onmousedown = (e) => {
+            switch (e.target.id) {
+                case 'upLeftBtn': {
+                    onDragDown(e, 'nw')
+                };
+                break
+            case 'upRightBtn': {
+                onDragDown(e, "ne");
+            };
+            break
+            case 'downLeftBtn': {
+                onDragDown(e, "sw");
+            };
+            break
+            case 'downRightBtn': {
+                onDragDown(e, "se");
+            };
+            break
+            }
+        }
+    })
+    // 鼠标移动
+    document.onmousemove = function (e) {
+        if (obj) {
+            e = e || window.event;
+            var location = {
+                x: e.x || e.clientX,
+                y: e.y || e.clientY
+            }
+            switch (obj.operateType) {
+                case "nw":
+                    move('n', location, box);
+                    move('w', location, box);
+                    break;
+                case "ne":
+                    move('n', location, box);
+                    move('e', location, box);
+                    break;
+                case "sw":
+                    move('s', location, box);
+                    move('w', location, box);
+                    break;
+                case "se":
+                    move('s', location, box);
+                    move('e', location, box);
+                    break;
+                case "move":
+                    move('move', location, box);
+                    break;
+            }
+        }
+    }
+    // 清除移动
+    document.onmouseup = function () {
+        obj = null;
+    }
+    //鼠标点击
+    var onDragDown = function (e, type) {
+        e.stopPropagation();
+        e = e || window.event;
+        clickX = e.x || e.clientX;
+        clickY = e.y || e.clientY;
+        obj = this;
+        obj.operateType = type;
+        box = document.getElementById("box");
+    }
+    // 鼠标移动
+    var move = function (type, location, tarobj) {
+        switch (type) {
+            case 'n':
+                var add_length = clickY - location.y;
+                clickY = location.y;
+                var length = parseInt(tarobj.style.height) + add_length;
+                tarobj.style.height = length + "px";
+                tarobj.style.top = (clickY - 100) + "px";
+                break;
+            case 's':
+                var add_length = clickY - location.y;
+                clickY = location.y;
+                var length = parseInt(tarobj.style.height) - add_length;
+                tarobj.style.height = length + "px";
+                break;
+            case 'w':
+                var add_length = clickX - location.x;
+                clickX = location.x;
+                var length = parseInt(tarobj.style.width) + add_length;
+                tarobj.style.width = length + "px";
+                tarobj.style.left = clickX + "px";
+                break;
+            case 'e':
+                var add_length = clickX - location.x;
+                clickX = location.x;
+                var length = parseInt(tarobj.style.width) - add_length;
+                tarobj.style.width = length + "px";
+                break;
+            case 'move':
+                var add_ylength = clickY - location.y;
+                clickY = location.y;
+                var length = parseInt(tarobj.style.top) - add_ylength;
+                tarobj.style.top = length + "px";
+                var add_xlength = clickX - location.x;
+                clickX = location.x;
+                var xlength = parseInt(tarobj.style.left) - add_xlength;
+                tarobj.style.left = xlength + "px";
+                break;
+        }
+    }
+}
+/**
+ * 清除字符串前后的空格。（兼容所有浏览器，且不能使用trim()）
+ * @param {字符串} str 
+ */
+function trim(str) {
+    return str.replace(/^\s+/, "").replace(/\s+$/, "");
 }
